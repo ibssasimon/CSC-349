7e1cb95c74e44741382247e468034b8057d3a095
Simon Ibssa <ibssasimon@gmail.com>
2020-03-31 15:35:25 -0700

    init commit

---
+++ TestFile.java
@@ -0,0 +1,14 @@
+public class TestFile {
+	public static void main(String[] args) {
+		System.out.println("Hello world, welcome to Algorithms");
+		
+
+		int i = 0;
+		if (i == 0) {
+			System.out.println("i is: " + i);
+		}
+	}
+
+
+
+}
