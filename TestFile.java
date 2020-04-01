73bf5238de658fbf2c35550e919d170456415aad
Simon Ibssa <ibssasimon@Simons-iMac.attlocal.net>
2020-03-31 23:12:10 -0700

    updating stuff

--- TestFile.java
+++ TestFile.java
@@ -6,9 +6,19 @@ public class TestFile {
 		int i = 0;
 		if (i == 0) {
 			System.out.println("i is: " + i);
+			System.out.println("Yippee!");
 		}
+		printNums();
 	}
 
+	private static void printNums() {
+		int k = 1;
+
+		for(int i = 0; i < 5; i++) {
+			k++;
+			System.out.println("K is now: " + k);
+		}
+	}
 
 
 }
